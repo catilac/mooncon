8af108f597a3a3e825a6a85ee9f9e2f7e98c9a75
catilac <moon@softmoon.world>
2025-05-09 18:21:27 -0400

    Created MoonContext; Resulting need for lua registry

--- main.c
+++ main.c
@@ -6,9 +6,20 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
-static lua_State *L = NULL;
+static const char moon_context_key;
+
+typedef struct MoonContext {
+   SDL_Window *window;
+   SDL_Renderer *renderer;
+   lua_State *L;
+} MoonContext;
+
+MoonContext *getMoonContext(lua_State *L)
+{
+   lua_pushlightuserdata(L, (void *)&moon_context_key);
+   lua_gettable(L, LUA_REGISTRYINDEX);
+   return lua_touserdata(L, -1);
+}
 
 int clear(lua_State *L)
 {
@@ -16,8 +27,10 @@ int clear(lua_State *L)
    uint8_t g = luaL_checkinteger(L, 2);
    uint8_t b = luaL_checkinteger(L, 3);
 
-   SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-   SDL_RenderClear(renderer);
+   MoonContext *ctx = getMoonContext(L);
+
+   SDL_SetRenderDrawColor(ctx->renderer, r, g, b, 255);
+   SDL_RenderClear(ctx->renderer);
 
    return 1;
 }
@@ -28,7 +41,9 @@ int color(lua_State *L)
    uint8_t g = luaL_checkinteger(L, 2);
    uint8_t b = luaL_checkinteger(L, 3);
 
-   SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+   MoonContext *ctx = getMoonContext(L);
+
+   SDL_SetRenderDrawColor(ctx->renderer, r, g, b, 255);
 
    return 1;
 }
@@ -43,7 +58,10 @@ int rect(lua_State *L)
    r.x = xpos;
    r.y = ypos;
    r.w = r.h = size * 4.0f; // TODO: Figure out global scale for our coordinate system
-   SDL_RenderFillRect(renderer, &r);
+                            //
+   MoonContext *ctx = getMoonContext(L);
+
+   SDL_RenderFillRect(ctx->renderer, &r);
 
    return 1;
 }
@@ -60,14 +78,16 @@ int text(lua_State *L)
    float x, y;
    const float scale = 4.0f;
 
+   MoonContext *ctx = getMoonContext(L);
+
    /* Center the message and scale it up */
-   SDL_GetRenderOutputSize(renderer, &w, &h);
-   SDL_SetRenderScale(renderer, scale, scale);
+   SDL_GetRenderOutputSize(ctx->renderer, &w, &h);
+   SDL_SetRenderScale(ctx->renderer, scale, scale);
    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(str)) / 2;
    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
 
    /* Draw the message */
-   SDL_RenderDebugText(renderer, x, y, str);
+   SDL_RenderDebugText(ctx->renderer, x, y, str);
 
    return 1;
 }
@@ -75,44 +95,57 @@ int text(lua_State *L)
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
 
+   MoonContext *ctx = (MoonContext *)SDL_calloc(1, sizeof(MoonContext));
+   if (!ctx)
+      return SDL_APP_FAILURE;
+
+   *appstate = ctx;
+
    /* Lua */
-   L = luaL_newstate();
-   luaL_openlibs(L);
+   ctx->L = luaL_newstate();
+   luaL_openlibs(ctx->L);
 
-   lua_pushnumber(L, 800);
-   lua_setglobal(L, "width");
+   lua_pushlightuserdata(ctx->L, (void *)&moon_context_key);
+   lua_pushlightuserdata(ctx->L, ctx);
+   lua_settable(ctx->L, LUA_REGISTRYINDEX);
 
-   lua_pushnumber(L, 600);
-   lua_setglobal(L, "height");
+   lua_pushnumber(ctx->L, 800);
+   lua_setglobal(ctx->L, "width");
 
-   lua_pushcfunction(L, clear);
-   lua_setglobal(L, "clear");
+   lua_pushnumber(ctx->L, 600);
+   lua_setglobal(ctx->L, "height");
 
-   lua_pushcfunction(L, text);
-   lua_setglobal(L, "text");
+   lua_pushcfunction(ctx->L, clear);
+   lua_setglobal(ctx->L, "clear");
 
-   lua_pushcfunction(L, rect);
-   lua_setglobal(L, "rect");
+   lua_pushcfunction(ctx->L, text);
+   lua_setglobal(ctx->L, "text");
 
-   lua_pushcfunction(L, color);
-   lua_setglobal(L, "color");
+   lua_pushcfunction(ctx->L, rect);
+   lua_setglobal(ctx->L, "rect");
 
-   if (luaL_dofile(L, "./prog.lua") == LUA_OK)
+   lua_pushcfunction(ctx->L, color);
+   lua_setglobal(ctx->L, "color");
+
+   if (luaL_dofile(ctx->L, "./prog.lua") == LUA_OK)
    {
       printf("prog.lua: OK");
    } else { 
-      luaL_error(L, "Error: %s\n", lua_tostring(L, -1));
+      luaL_error(ctx->L, "Error: %s\n", lua_tostring(ctx->L, -1));
    }
 
    /* Window */
-   if (!SDL_CreateWindowAndRenderer("MoonCon", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
+   if (!SDL_CreateWindowAndRenderer("MoonCon", 
+                                    800, 600, SDL_WINDOW_FULLSCREEN, 
+                                    &ctx->window, &ctx->renderer))
+   {
       SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
       return SDL_APP_FAILURE;
    }
 
    /* Call Init() */
-   lua_getglobal(L, "init");
-   if (lua_pcall(L, 0, 0, 0) != 0) {
+   lua_getglobal(ctx->L, "init");
+   if (lua_pcall(ctx->L, 0, 0, 0) != 0) {
       printf("ERROR calling init\n");
    }
 
@@ -130,24 +163,26 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+   MoonContext *ctx = (MoonContext *)appstate;
 
-   lua_getglobal(L, "update");
-   if (lua_pcall(L, 0, 0, 0) != 0) {
+   lua_getglobal(ctx->L, "update");
+   if (lua_pcall(ctx->L, 0, 0, 0) != 0) {
       printf("ERROR calling update\n");
    }
-   lua_getglobal(L, "draw");
-   if (lua_pcall(L, 0, 0, 0) != 0) {
-      const char *error = lua_tostring(L, -1);
+   lua_getglobal(ctx->L, "draw");
+   if (lua_pcall(ctx->L, 0, 0, 0) != 0) {
+      const char *error = lua_tostring(ctx->L, -1);
       fprintf(stderr, "Error running draw: %s\n", error);
-      lua_pop(L, 1); // remove error message
+      lua_pop(ctx->L, 1); // remove error message
    }
 
-   SDL_RenderPresent(renderer);
+   SDL_RenderPresent(ctx->renderer);
 
    return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-   lua_close(L);
+   MoonContext *ctx = (MoonContext *)appstate;
+   lua_close(ctx->L);
 }
