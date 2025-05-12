aeb8feac6d700a8989db9cb9606528aedf4de047
catilac <moon@softmoon.world>
2025-05-12 14:00:52 -0400

    wip

--- src/main.c
+++ src/main.c
@@ -1,10 +1,40 @@
 #define SDL_MAIN_USE_CALLBACKS 1
-#include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <stdio.h>
+
 #include "moon_context.h"
 #include "ilua.h"
+#include "editbox.h"
+
+static EditBox *editor = NULL;
+static TTF_Font *font = NULL;
+static SDL_Surface *window_surface = NULL;
+static SDL_FRect editRect = {50, 50, 200, 150};
+
+// TODO: put this somewhere out of the way, maybe just read
+// from `prog.lua` or something.
+const char *init_program = "\
+function init()\n \
+\tsize = 15\n \
+\tt = 0\n \
+end\n \
+\n \
+function update()\n \
+\tt = t + 0.01\n \
+\tsize = 15 * math.sin(t);\n \
+end\n \
+\n \
+function draw()\n \
+\tclear(55, 55, 225)\n \
+\tcolor(200, 150, 25)\n \
+\ttext('Hello, MoonCon User!', 1)\n \
+\trect(40, 40, size)\n \
+\n \
+\tcolor(40, 240, 120)\n \
+\trect(50 + math.cos(t*5) * 15, 50 + math.sin(t*8) * 24, 10);\n \
+end";
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -16,12 +46,27 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
    /* Window */
    if (!SDL_CreateWindowAndRenderer("MoonCon", 
-                                    800, 600, SDL_WINDOW_FULLSCREEN, 
+                                    800, 600, 0/*SDL_WINDOW_FULLSCREEN*/,
                                     &ctx->window, &ctx->renderer))
    {
       SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
       return SDL_APP_FAILURE;
    }
+
+   window_surface = SDL_GetWindowSurface(ctx->window);
+
+   TTF_Init();
+   font = TTF_OpenFont("fonts/Kobold.ttf", 24.0f);
+   if (!font) {
+      SDL_Log("Font: %s\n", SDL_GetError());
+      return SDL_APP_FAILURE;
+   }
+   
+   editor = EditBox_Create(ctx->window, ctx->renderer,
+                           TTF_CreateRendererTextEngine(ctx->renderer),
+                           font, &editRect);
+   EditBox_SetFocus(editor, true);
+   EditBox_Insert(editor, init_program);
 
    /* Call Init() */
    if (iLuaCallFunc(ctx->L, "init") != 0) {
@@ -34,11 +79,18 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-   if (event->type == SDL_EVENT_KEY_DOWN ||
-       event->type == SDL_EVENT_QUIT) 
+   switch (event->type)
    {
-      return SDL_APP_SUCCESS;
+      case SDL_EVENT_KEY_DOWN:
+         EditBox_HandleEvent(editor, event);
+         break;
+      case SDL_EVENT_QUIT:
+         return SDL_APP_SUCCESS;
+      default:
+         EditBox_HandleEvent(editor, event);
+         break;
    }
+
    return SDL_APP_CONTINUE;
 }
 
@@ -56,6 +108,10 @@ SDL_AppResult SDL_AppIterate(void *appstate)
       fprintf(stderr, "Error running draw: %s\n", error);
       lua_pop(ctx->L, 1); // remove error message
    }
+
+   SDL_RenderRect(ctx->renderer, &editRect);
+   TTF_SetTextColor(editor->text, 0, 0, 0, 255);
+   EditBox_Draw(editor);
 
    SDL_RenderPresent(ctx->renderer);
 
